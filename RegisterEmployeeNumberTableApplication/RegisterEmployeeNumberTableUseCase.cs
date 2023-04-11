51c92b1b8116d343c73f3adcf187fc1314f6c6b8
Matazoro-Kemtari <k_hirano@wadass.com>
2023-04-11 16:27:10 +0900

    社員番号対応表を登録するUseCaseの枠組み追加

---
+++ RegisterEmployeeNumberTableApplication/RegisterEmployeeNumberTableUseCase.cs
@@ -0,0 +1,19 @@
+﻿namespace RegisterEmployeeNumberTableApplication;
+
+public interface IRegisterEmployeeNumberTableUseCase
+{
+    /// <summary>
+    /// 社員番号対応表を登録する
+    /// </summary>
+    /// <param name="csvPath"></param>
+    /// <returns></returns>
+    Task ExecuteAsync(string csvPath);
+}
+
+public class RegisterEmployeeNumberTableUseCase : IRegisterEmployeeNumberTableUseCase
+{
+    public async Task ExecuteAsync(string csvPath)
+    {
+
+    }
+}
