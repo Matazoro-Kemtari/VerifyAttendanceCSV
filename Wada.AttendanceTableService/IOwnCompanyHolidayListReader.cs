be9ec695f50b11510c7a332a1db0e281f6448833
Matazoro-Kemtari <k_hirano@wadass.com>
2023-04-17 11:26:49 +0900

    休日カレンダー読み込み処理インフラ層追加

---
+++ Wada.AttendanceTableService/IOwnCompanyHolidayListReader.cs
@@ -0,0 +1,14 @@
+﻿using Wada.Data.DesignDepartmentDataBase.Models.OwnCompanyCalendarAggregation;
+
+namespace Wada.AttendanceTableService;
+
+public interface IOwnCompanyHolidayListReader
+{
+    /// <summary>
+    /// 自社カレンダーを読み込む
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <param name="calendarGroupName"></param>
+    /// <returns></returns>
+    Task<IEnumerable<OwnCompanyHoliday>> ReadAllAsync(Stream stream, string calendarGroupName);
+}
