e296178b16fed4e26cdddd2eba38761c540ca000
Matazoro-Kemtari <k_hirano@wadass.com>
2023-05-02 10:36:34 +0900

    Directoryリストが無指定のときのボタン制御追加

---
+++ Wada.VerifyAttendanceCSV/Views/ValueConverterGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Data;
+
+namespace Wada.VerifyAttendanceCSV.Views
+{
+    /// <summary>
+    /// 複数のコンバーターをまとめるコンバーター
+    /// </summary>
+    public class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
